c2aed4d9edcfedd0be71dffd742f937c38e381d6
Davi Seidel Brandão <daviseidel.brandao@gmail.com>
2023-12-28 19:12:57 -0300

    Added XGrabKey

--- xlib.go
+++ xlib.go
@@ -1,13 +1,10 @@
-
 //          Copyright 2016 Vitali Baumtrok
 // Distributed under the Boost Software License, Version 1.0.
 //      (See accompanying file LICENSE or copy at
 //        http://www.boost.org/LICENSE_1_0.txt)
 
-
 // Binding of Xlib (version 11, release 6.7).
 package xlib
-
 
 // #cgo LDFLAGS: -lX11
 // #include <stdlib.h>
@@ -21,19 +18,20 @@ import (
 type Display C.Display
 type Screen C.Screen
 type Window C.Window
+type Bool C.Bool
 
-func strConcat ( a []interface{} ) string {
+func strConcat(a []interface{}) string {
 	str := ""
 	for _, strPart := range a {
 		switch s := strPart.(type) {
-			case string:
-				str += s
+		case string:
+			str += s
 		}
 	}
 	return str
 }
 
-func XOpenDisplay ( displayNameParts ...interface{} ) *Display {
+func XOpenDisplay(displayNameParts ...interface{}) *Display {
 	if len(displayNameParts) == 0 {
 		display := C.XOpenDisplay(nil)
 		return (*Display)(display)
@@ -53,12 +51,12 @@ func XOpenDisplay ( displayNameParts ...interface{} ) *Display {
 	}
 }
 
-func XCloseDisplay ( display *Display ) {
+func XCloseDisplay(display *Display) {
 	displayC := (*C.Display)(display)
 	C.XCloseDisplay(displayC)
 }
 
-func XDisplayString ( display *Display ) string {
+func XDisplayString(display *Display) string {
 	displayC := (*C.Display)(display)
 	displayNameC := C.XDisplayString(displayC)
 	displayName := C.GoString(displayNameC)
@@ -66,43 +64,43 @@ func XDisplayString ( display *Display ) string {
 	return displayName
 }
 
-func XScreenCount ( display *Display ) int {
+func XScreenCount(display *Display) int {
 	displayC := (*C.Display)(display)
 	screenCount := C.XScreenCount(displayC)
 	return int(screenCount)
 }
 
-func XScreenOfDisplay ( display *Display, screenNumber int ) *Screen {
+func XScreenOfDisplay(display *Display, screenNumber int) *Screen {
 	displayC := (*C.Display)(display)
 	screen := C.XScreenOfDisplay(displayC, C.int(screenNumber))
 	return (*Screen)(screen)
 }
 
-func XWidthOfScreen ( screen *Screen ) int {
+func XWidthOfScreen(screen *Screen) int {
 	screenC := (*C.Screen)(screen)
 	width := C.XWidthOfScreen(screenC)
 	return int(width)
 }
 
-func XHeightOfScreen ( screen *Screen ) int {
+func XHeightOfScreen(screen *Screen) int {
 	screenC := (*C.Screen)(screen)
 	height := C.XHeightOfScreen(screenC)
 	return int(height)
 }
 
-func XDefaultScreenOfDisplay ( display *Display ) *Screen {
+func XDefaultScreenOfDisplay(display *Display) *Screen {
 	displayC := (*C.Display)(display)
 	defaultScreen := C.XDefaultScreenOfDisplay(displayC)
 	return (*Screen)(defaultScreen)
 }
 
-func XRootWindowOfScreen ( screen *Screen ) Window {
+func XRootWindowOfScreen(screen *Screen) Window {
 	screenC := (*C.Screen)(screen)
 	rootWindow := C.XRootWindowOfScreen(screenC)
 	return Window(rootWindow)
 }
 
-func XCreateSimpleWindow ( display *Display, parent Window, x, y int, width, height, borderWidth uint, border, background uint64) Window {
+func XCreateSimpleWindow(display *Display, parent Window, x, y int, width, height, borderWidth uint, border, background uint64) Window {
 	displayC := (*C.Display)(display)
 	windowC := (C.Window)(parent)
 	xC := C.int(x)
@@ -116,16 +114,28 @@ func XCreateSimpleWindow ( display *Display, parent Window, x, y int, width, hei
 	return Window(window)
 }
 
-func XMapWindow ( display *Display, window Window) {
+func XMapWindow(display *Display, window Window) {
 	displayC := (*C.Display)(display)
 	windowC := (C.Window)(window)
 	C.XMapWindow(displayC, windowC)
 }
 
-func XSelectInput ( display *Display, window Window, eventMask int64) {
+func XSelectInput(display *Display, window Window, eventMask int64) {
 	displayC := (*C.Display)(display)
 	windowC := (C.Window)(window)
 	eventMaskC := (C.long)(eventMask)
-	C.XSelectInput ( displayC, windowC, eventMaskC )
+	C.XSelectInput(displayC, windowC, eventMaskC)
 }
 
+func XGrabKey(display *Display, keycode int, modifiers int, grab_window Window, owner_events Bool, pointer_mode int, keyboard_mode int) int {
+	displayC := (*C.Display)(display)
+	keycodeC := (C.int)(keycode)
+	modifiersC := (C.uint)(modifiers)
+	grab_windowC := (C.Window)(grab_window)
+	owner_eventsC := (C.Bool)(owner_events)
+	pointer_modeC := (C.int)(pointer_mode)
+	keyboard_modeC := (C.int)(keyboard_mode)
+	status := C.XGrabKey(displayC, keycodeC, modifiersC, grab_windowC, owner_eventsC, pointer_modeC, keyboard_modeC)
+	return int(status)
+
+}
