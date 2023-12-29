d0b16782ca6ca0648aa280c0159d7bd114fe44ba
Davi Seidel Brandão <daviseidel.brandao@gmail.com>
2023-12-28 21:11:14 -0300

    Added GrabModeSync and Async

--- events.go
+++ events.go
@@ -1,23 +1,18 @@
-
 //          Copyright 2016 Vitali Baumtrok
 // Distributed under the Boost Software License, Version 1.0.
 //      (See accompanying file LICENSE or copy at
 //        http://www.boost.org/LICENSE_1_0.txt)
 
-
 // Binding of Xlib (version 11, release 6.7).
 package xlib
-
 
 // #cgo LDFLAGS: -lX11
 // #include <stdlib.h>
 // #include <X11/Xlib.h>
 // #include "xlib.h"
 import "C"
-import (
-	//"unsafe"
-)
 
+//"unsafe"
 
 const (
 	Expose     = int(C.Expose)
@@ -52,6 +47,8 @@ const (
 	PropertyChangeMask       = int64(C.PropertyChangeMask)
 	ColormapChangeMask       = int64(C.ColormapChangeMask)
 	OwnerGrabButtonMask      = int64(C.OwnerGrabButtonMask)
+	GrabModeSync             = int64(C.GrabModeSync)
+	GrabModeAsync            = int64(C.GrabModeAsync)
 )
 
 type XEvent interface {
@@ -64,53 +61,70 @@ type tEventType struct {
 
 type XKeyEvent struct {
 	tEventType
-	Serial uint64
-	SendEvent bool
-	Display *Display 
-	Window Window
-	Root Window
-	Subwindow Window
-	Time uint64
-	X, Y int
+	Serial       uint64
+	SendEvent    bool
+	Display      *Display
+	Window       Window
+	Root         Window
+	Subwindow    Window
+	Time         uint64
+	X, Y         int
 	XRoot, YRoot int
-	State uint
-	KeyCode uint
-	SameScreen bool
+	State        uint
+	KeyCode      uint
+	SameScreen   bool
 }
 
-func XNextEvent ( display *Display ) XEvent {
+func XNextEvent(display *Display) XEvent {
 	displayC := (*C.Display)(display)
 	var xeventC C.XEvent
 	var xeventTypeC C.int
-	C.XNextEvent(displayC,&xeventC)
-	C.xlib_xevent_type(&xeventC,&xeventTypeC)
+	C.XNextEvent(displayC, &xeventC)
+	C.xlib_xevent_type(&xeventC, &xeventTypeC)
 
 	switch xeventTypeC {
-		case C.KeyPress:
-			return newXKeyEvent(&xeventC,xeventTypeC)
-		case C.KeyRelease:
-			return newXKeyEvent(&xeventC,xeventTypeC)
+	case C.KeyPress:
+		return newXKeyEvent(&xeventC, xeventTypeC)
+	case C.KeyRelease:
+		return newXKeyEvent(&xeventC, xeventTypeC)
 	}
 	return nil
 }
 
-func newXKeyEvent ( xeventC *C.XEvent, xeventTypeC C.int ) *XKeyEvent {
+func newXKeyEvent(xeventC *C.XEvent, xeventTypeC C.int) *XKeyEvent {
 	xKeyEvent := new(XKeyEvent)
-	var serialC C.ulong; var sendEventC C.Bool; var displayC *C.Display; var windowC C.Window
-	var rootC C.Window; var subwindowC C.Window; var timeC C.Time; var xC, yC C.int
-	var xRootC, yRootC C.int; var stateC C.uint; var keyCodeC C.uint; var sameScreenC C.Bool
-	C.xlib_xkeyevent_values(xeventC,&serialC,&sendEventC,&displayC,&windowC,&rootC,&subwindowC,
-	                                &timeC,&xC,&yC,&xRootC,&yRootC,&stateC,&keyCodeC,&sameScreenC)
-	xKeyEvent.typeCode = int(xeventTypeC); xKeyEvent.Serial = uint64(serialC)
-	xKeyEvent.SendEvent = (sendEventC != 0); xKeyEvent.Display = (*Display)(displayC)
-	xKeyEvent.Window = Window(windowC); xKeyEvent.Root = Window(rootC)
-	xKeyEvent.Subwindow = Window(subwindowC); xKeyEvent.Time = uint64(timeC)
-	xKeyEvent.X = int(xC); xKeyEvent.Y = int(yC); xKeyEvent.XRoot = int(xRootC); xKeyEvent.YRoot = int(yRootC)
-	xKeyEvent.State = uint(stateC); xKeyEvent.KeyCode = uint(keyCodeC); xKeyEvent.SameScreen = (sameScreenC != 0)
+	var serialC C.ulong
+	var sendEventC C.Bool
+	var displayC *C.Display
+	var windowC C.Window
+	var rootC C.Window
+	var subwindowC C.Window
+	var timeC C.Time
+	var xC, yC C.int
+	var xRootC, yRootC C.int
+	var stateC C.uint
+	var keyCodeC C.uint
+	var sameScreenC C.Bool
+	C.xlib_xkeyevent_values(xeventC, &serialC, &sendEventC, &displayC, &windowC, &rootC, &subwindowC,
+		&timeC, &xC, &yC, &xRootC, &yRootC, &stateC, &keyCodeC, &sameScreenC)
+	xKeyEvent.typeCode = int(xeventTypeC)
+	xKeyEvent.Serial = uint64(serialC)
+	xKeyEvent.SendEvent = (sendEventC != 0)
+	xKeyEvent.Display = (*Display)(displayC)
+	xKeyEvent.Window = Window(windowC)
+	xKeyEvent.Root = Window(rootC)
+	xKeyEvent.Subwindow = Window(subwindowC)
+	xKeyEvent.Time = uint64(timeC)
+	xKeyEvent.X = int(xC)
+	xKeyEvent.Y = int(yC)
+	xKeyEvent.XRoot = int(xRootC)
+	xKeyEvent.YRoot = int(yRootC)
+	xKeyEvent.State = uint(stateC)
+	xKeyEvent.KeyCode = uint(keyCodeC)
+	xKeyEvent.SameScreen = (sameScreenC != 0)
 	return xKeyEvent
 }
 
-func (this *tEventType) Type ( ) int {
+func (this *tEventType) Type() int {
 	return this.typeCode
 }
-
